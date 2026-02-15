7d756920dae3fba7b1172af7b71fcd9c3f8b122e
frederik-sm-andersen <2806099@rts-365.dk>
2026-02-15 19:43:18 +0100

    have made some css and adet more data

--- js/data.js
+++ js/data.js
@@ -3,7 +3,8 @@ let hero = {
     image: "https://dazzling-squirrel-77a63d.netlify.app/imgs/hero.png",
     headline: "Find out how we can save your time in Backpacking in Europe",
     copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac amet id eget scelerisque amet etiam in sit commodo. Pretium ut duis orci pulvinar pretium id consectetur.",
-    icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/Globe.png"
+    icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/Globe.png",
+    buttonText: "Explore",
 }
 
 
@@ -19,7 +20,7 @@ let services = [
         headline: "Additional services designed for the needs of campers",
         text: "Travelling often means that you dont have time to organise excursions along your route. EasyCamper takes care of that!",
         linktext: "Discover the Guided Tours and additional services that you can link to your structure"
-    }, 
+    },
     {
         illustration: "https://dazzling-squirrel-77a63d.netlify.app/imgs/illustrations/destination.png",
         headline: "Free online showcase with promotion in Italy and abroad",
@@ -30,26 +31,30 @@ let services = [
 
 let facilities = {
     headline: "Making your facility known is our priority",
-    options : [
+    options: [
         {
             icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/list-check.png",
             headline: "Choose the type of stop",
-            text: "Select the type of stopover you are ordering from Equipped Area, Camping and Agricamping"
+            text: "Select the type of stopover you are ordering from Equipped Area, Camping and Agricamping",
+            linktext: "show me more"
         },
         {
             icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/spellcheck.png",
             headline: "Describe your structure",
-            text: "Describe thee property, location and nearby attractions"
+            text: "Describe thee property, location and nearby attractions",
+            linktext: "show me more"
         },
         {
             icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/Text-Frame-Add.png",
             headline: "Build your showcase",
-            text: "Add photos, videos and descriptions Define the price of each type of that make your propety truly"
+            text: "Add photos, videos and descriptions Define the price of each type of that make your propety truly",
+            linktext: "show me more"
         },
         {
             icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/Calendar-Today.png",
             headline: "Prices and Availability",
-            text: "pitch and configure the availablity calendar."
+            text: "pitch and configure the availablity calendar.",
+            linktext: "show me more"
         },
     ]
 }
@@ -58,6 +63,7 @@ let sites = {
     headline: "Join the community campervan sites managers.",
     text: "Discover all the equibbed areas in Italy. Find the shop with all the services for you and your camper.",
     btnicon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/arrow-icon.png",
+    buttomText: "Start",
     places: [
         {
             img: "https://dazzling-squirrel-77a63d.netlify.app/imgs/lucciano.png",
@@ -77,22 +83,75 @@ let sites = {
     ]
 }
 
-let advantages = [
-    {
-        icon:"https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/compare.png",
-        headline: "Convenience and Transparency",
-        text: "You don’t have to find out later or only after camenierk and trarsparent Easyamper ratains pliacas and beautias of your tariaary and halpin a fixed commission on each transaction of 5"
-    },
-    {
-        icon:"https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/library_books.png",
-        headline: "Digital marketing and editorial support",
-        text: "You don’t have to find out later or only after camenierk and trarsparent Easyamper ratains pliacas and beautias of your tariaary and halpin a fixed commission on each transaction of 5"
-    },
-    {
-        icon:"https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/Compare-HSV.png",
-        headline: "Community of services dedicated to campers",
-        text: "You don’t have to find out later or only after camenierk and trarsparent Easyamper ratains pliacas and beautias of your tariaary and halpin a fixed commission on each transaction of 5"
-    },
-]
+let advantages = {
+
+    headline: "Our Advantages",
+
+    ourAdvantages: [
+        {
+            icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/compare.png",
+            headline: "Convenience and Transparency",
+            text: "You don’t have to find out later or only after camenierk and trarsparent Easyamper ratains pliacas and beautias of your tariaary and halpin a fixed commission on each transaction of 5"
+        },
+        {
+            icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/library_books.png",
+            headline: "Digital marketing and editorial support",
+            text: "You don’t have to find out later or only after camenierk and trarsparent Easyamper ratains pliacas and beautias of your tariaary and halpin a fixed commission on each transaction of 5"
+        },
+        {
+            icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/Compare-HSV.png",
+            headline: "Community of services dedicated to campers",
+            text: "You don’t have to find out later or only after camenierk and trarsparent Easyamper ratains pliacas and beautias of your tariaary and halpin a fixed commission on each transaction of 5"
+        },
+    ]
+}
 
 // Opret dit dataobjekt til footerens indhold herunder
+
+
+let footer = {
+    text: "Easy Camper",
+    headline: "When Passion Meets Comfort.",
+
+    allUl: [
+        {
+            header: "Discover the Network",
+            headerList: [
+                { link: "#", text: "Where to shop" },
+                { link: "#", text: "Tour Guided" },
+                { link: "#", text: "Our Tours" },
+                { link: "#", text: "About Us" }
+            ]
+        },
+        {
+            header: "Become a Manager",
+            headerList: [
+                { link: "#", text: "Join your Pairing" },
+                { link: "#", text: "Offer your Service" },
+                { link: "#", text: "Are you a guide?" },
+            ]
+        },
+        {
+            header: "Assistance",
+            headerList: [
+                { link: "#", text: "Support Center" },
+                { link: "#", text: "Faqs" },
+                { link: "#", text: "Cancallation Options" },
+                { link: "#", text: "Reliablity and Sequrity" },
+            ]
+        }
+    ],
+
+    eazyCamper: "EazyCamper. All rights reserved.",
+    bottemUls: [
+        { link: "#", text: "aboute" },
+        { link: "#", text: "Site Map" },
+        { link: "#", text: "Terms" },
+        { link: "#", text: "Privacy Policy" },
+        { link: "#", text: "Teams" },
+
+    ],
+}
+
+
+
--- js/index.js
+++ js/index.js
@@ -5,18 +5,24 @@
 let heroHeader = document.querySelector(".hero");
 let serviceList = document.querySelector(".services");
 let facilitiesList = document.querySelector(".facilities");
-let siteslist = document.querySelector(".sites")
+let siteslist = document.querySelector(".sites");
 let advantageslist = document.querySelector(".advantages");
+let footerlist = document.querySelector(".footer");
 
 
-    const heroSection = 
+const heroSection =
     /* html*/`
     <section class="hero-div">
         <img class="hero-img" src=${hero.image}>
+        <article class="hero-article">
         <h1 class="hero-h1" >${hero.headline}</h1>
-        <p>${hero.copy}</p>
+        <p class="hero-text">${hero.copy}</p>
+        <button class="hero-button">
+            <p class="hero-btext">${hero.buttonText}</p>
+        </button>
+        </article>        
     </section> `
-    heroHeader.insertAdjacentHTML("beforeend",heroSection);
+heroHeader.insertAdjacentHTML("beforeend", heroSection);
 
 //-------------------------------------------------------------
 
@@ -24,63 +30,137 @@ services.forEach(service => {
     const serviceSection =
     /* html*/`
     <section class="services-section">
+        <figure class="servicse-figure">
         <img class="service-img" src=${service.illustration}>
+        </figure>               
         <h2 class="service-h2">${service.headline}</h2>
+        <p class="services-text">${service.text}</p>
         <a class="sevice-link-text" href="#">${service.linktext}</a>
     </section> `
-    serviceList.insertAdjacentHTML("beforeend",serviceSection)
+    serviceList.insertAdjacentHTML("beforeend", serviceSection)
 })
 
 
-    // let serviceImg = document.createElement("img");
-    // serviceImg.classList.add("service-img")
-    // serviceImg.src = services.illustration
-    
-    // let serviceheadline = document.createElement("h2");
-    // serviceheadline.classList.add("service-h2")
+// let serviceImg = document.createElement("img");
+// serviceImg.classList.add("service-img")
+// serviceImg.src = services.illustration
 
-    // let servicesLinkText = document.createElement("a")
-    // servicesLinkText.classList.add("sevice-link-text")
+// let serviceheadline = document.createElement("h2");
+// serviceheadline.classList.add("service-h2")
+
+// let servicesLinkText = document.createElement("a")
+// servicesLinkText.classList.add("sevice-link-text")
 
 
-    //--------------------------------------------------
+//--------------------------------------------------
 
-const facilitieSection = 
+const facilitieSection =
     /* html*/`
     <section class="facilitie-section">
         <h2 class="facilities-h2" >${facilities.headline}</h2>
     </section> `
-    facilitiesList.insertAdjacentHTML("beforeend",facilitieSection);
+facilitiesList.insertAdjacentHTML("beforeend", facilitieSection);
 
-    facilities.options.forEach(facility => {
-        const facilitiesArticle =
+facilities.options.forEach(facility => {
+    const facilitiesArticle =
     /* html*/`
-    <article>
-        
+    <article class="facilities-article">
         <img class="facilities-icon" src=${facility.icon}>
         <h3 class="facilities-h3">${facility.headline}</h3>
         <p class="facilities-text">${facility.text}</p>
+        <a class="facilities-linktext" href="#">${facility.linktext}</a>
     </article> `
-    facilitiesList.insertAdjacentHTML("beforeend",facilitiesArticle)
-    })
+    facilitiesList.insertAdjacentHTML("beforeend", facilitiesArticle)
+})
 
-    //---------------------------------------------------
+//---------------------------------------------------
 
 const sitesSection =
     /* html*/`
     <section>
-        <h2></h2>
+        <h2 class="sites-h2">${sites.headline}</h2>
+        <p class="sites-text">${sites.text}</p>
+        <button class="sites-buttom">
+        <img class="sites-starts" ${sites.btnicon}>
+        <p class="sites-buttomtext">${sites.buttomText}</p>
+        </button>
     </section>`
+siteslist.insertAdjacentHTML("beforeend", sitesSection)
 
-    //---------------------------------------------------
 
-advantages.forEach(advantages =>{
-    const advantage =
+sites.places.forEach(site => {
+    const sitesArticle =
+        /* html*/`
+        <article class="sites-article">
+            <figure class="sites-figure">
+            <img class="sites-img" src=${site.img}>
+            </figure>            
+            <h3 class="sites-h3">${site.name}</h3>
+            <a class="sites-city">${site.city}</a>
+        </article>`
+    siteslist.insertAdjacentHTML("beforeend", sitesArticle)
+})
+
+//---------------------------------------------------
+
+const advantageHeadline =
+    /*html*/`
+    <section class="advantages-s-h">
+        <h2 class="advantages-headline">${advantages.headline}</h2>
+    </section> `
+advantageslist.insertAdjacentHTML("beforeend", advantageHeadline),
+
+
+    advantages.ourAdvantages.forEach(advantag => {
+        const advantage =
     /* html*/`
     <section class="advantages-Article">
-        <img class="advantages-img" src=${advantages.icon}>
-        <h3 class="advantages-h3">${advantages.headline}</h3>
-        <p class="advantages-text">${advantages.text}</p>
+        <img class="advantages-img" src=${advantag.icon}>
+        <h3 class="advantages-h3">${advantag.headline}</h3>
+        <p class="advantages-text">${advantag.text}</p>
     </section> `
-    advantageslist.insertAdjacentHTML("beforeend",advantage)
+        advantageslist.insertAdjacentHTML("beforeend", advantage)
+    })
+
+//---------------------------------------------------
+
+
+const footersection =
+    /* html*/`
+    <section class="">
+        <h2 class="footer-h2">${footer.text}</h2>
+        <p class="footer-headline-t">${footer.headline}</p>
+    </section>`
+footerlist.insertAdjacentHTML("beforeend", footersection)
+
+
+footer.allUl.forEach(foot => {
+
+
+    const footerHeadline =
+    /* html*/`
+    <section class="footer-ec-section">
+        <h3>${foot.header}</h3>
+        <ul class="footer-ul-es">
+            ${foot.headerList.map(li => `<li class="footer-li-es"><a href="#">${li.text}</a></li>`).join("")}
+
+        </ul>
+    </section>`
+    footerlist.insertAdjacentHTML("beforeend", footerHeadline)
 })
+const newli = document.createElement("section")
+
+const footerBottemText =
+    /* html*/`
+    <section>
+        <h3>${footer.eazyCamper}</h3>
+        <ul class="footer-bottem-ul">
+            ${footer.bottemUls.map(bottemLi => `<li><a href="#">${bottemLi.text}</a></li>`).join("")}
+        </ul>
+    </section>`
+
+
+footerlist.insertAdjacentHTML("beforeend", footerBottemText)
+
+
+
